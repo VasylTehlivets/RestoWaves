ff02370edc09e73461d8664d47b5a96df2e1854b
VasylTehlivets <vasyltehlivets@gmail.com>
2024-01-02 15:07:00 +0200

    Create models.js

---
+++ models.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+// Оголошуємо модель продукту
+const Product = sequelize.define("Product", {
+  model: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  article: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  sizes: {
+    type: DataTypes.JSON, // Може бути JSON або ARRAY, залежно від потреб.
+    allowNull: true,
+  },
+});
+
+module.exports = Product;
